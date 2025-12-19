57c5d5965b9b17e1ce56d63254ea16bdb76982fe
Alimt36 <amtaaheri@gmail.com>
2025-12-19 15:21:25 +0330

    documentation added!

--- code.py
+++ code.py
@@ -1,20 +1,20 @@
 
 from __future__ import annotations
-import numpy as np 
-import pandas as pd
 import matplotlib.pyplot as plt 
 import math
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 #---------------------------------------------------------------------------------------------------------------------------
-points = []   
-# points_by_distance = []
+points = [] # every element of it is a point with 4 atributes
 #---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
-class vector_ : 
+class Linear_Algebra : 
 
+#---------------------------------------------------------------------------------------------------------------------------
+# the objects get made by 4 atributes : (x, y, z, classification) ---> basicaly a datastructure!
+# and then the objects get added to a global list (points) for easier access
 #---------------------------------------------------------------------------------------------------------------------------
     def __init__(self , x  , y , z=0 , classification=None ):
         self.x = x 
@@ -32,8 +32,18 @@ class vector_ :
 #---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
+# a static function that gets two objects of type class (Linear_Algebra) and calculate the distance between them
+#   ---> it has 7 diffrent modes that by the theory of linear algebra they have seperate meanings and usages
+#       ---> 0 : Euclidean distance
+#            1 : Manhattan Distance 
+#            2 : Chebyshev Distance
+#            3 : Minkowski Distance
+#            4 : Cosine Distance
+#            5 : Squared Euclidean Distance
+#            6 : Canberra Distance
+#---------------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def calculate_distance(mode=0 , p0:vector_=None , p1:vector_=None ) -> float :
+    def calculate_distance(mode=0 , p0:Linear_Algebra=None , p1:Linear_Algebra=None ) -> float :
         # Euclidean distance 
         if (mode == 0): 
             return math.sqrt( (abs(p0.x - p1.x)**2) + (abs(p0.y - p1.y)**2) + (abs(p0.z - p1.z)**2) )
@@ -50,9 +60,8 @@ class vector_ :
             p = 3
             return (abs(p0.x - p1.x)**p + abs(p0.y - p1.y)**p + abs(p0.z - p1.z)**p)**(1/p)
         
-        # Mode 4: Cosine Distance
+        # Cosine Distance
         elif mode == 4:
-            # Inner product <p0, p1>
             inner_product = p0.x * p1.x + p0.y * p1.y + p0.z * p1.z
 
             norm_p0 = math.sqrt(p0.x**2 + p0.y**2 + p0.z**2)
@@ -65,14 +74,14 @@ class vector_ :
             
             return 1 - cosine_similarity
         
-        # Mode 5: Squared Euclidean Distance
+        # Squared Euclidean Distance
         elif mode == 5:
             diff_x = p0.x - p1.x
             diff_y = p0.y - p1.y
             diff_z = p0.z - p1.z
             return diff_x**2 + diff_y**2 + diff_z**2
         
-        # Mode 6: Canberra Distance
+        # Canberra Distance
         elif mode == 6:
             sum_dist = 0
             coords = [(p0.x, p1.x), (p0.y, p1.y), (p0.z, p1.z)]
@@ -86,6 +95,8 @@ class vector_ :
 #---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
+# a function capable of printing the list (points_by_distance) in a neat way
+#---------------------------------------------------------------------------------------------------------------------------
     @staticmethod
     def print_by_distance(points_by_distance) -> None : 
         # print(points_by_distance)
@@ -96,20 +107,39 @@ class vector_ :
 #---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
+# K-NN : 
+#   ---> inputs : 
+#       k : number of near neighbors to calculate the classification
+#       v_to_classify :‌ is the obeject of the point that we want to clssify
+#       points : is the list that holds the points of the dataset 
+#       distance_mode : is the control varible of the function (calculate_distance()) 
+#
+#   ---> outputs : 
+#       it returns two values : 
+#           temp.index(max(temp)) : is the class number that the KNN has calculated.
+#           points_by_distance : is a list that gets made by the function and is like : ( (a points list object) , (distance of the object form the new point) )
+#
+#   ---> how it works : 
+#           calculates distance of all the points from the new point 
+#           make the list (points_by_distance) 
+#           sort the list (points_by_distance) by distance 
+#           calculate all the distinct classes in the dataset
+#           check the class of the k nearest neighbors and classify
+#---------------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def KNN( k=3 , v_to_classify:vector_=None , points=None , distance_mode=0 ) :
+    def KNN( k=3 , v_to_classify:Linear_Algebra=None , points=None , distance_mode=0 ) :
 
         # global points_by_distance
         points_by_distance = []
 
         for i in range (0 , len(points)) :
-            distance = vector_.calculate_distance( distance_mode , points[i] , v_to_classify)
+            distance = Linear_Algebra.calculate_distance( distance_mode , points[i] , v_to_classify)
             if (distance != 0):
                 points_by_distance.append((points[i] , distance))
             
         points_by_distance.sort(key=lambda x: x[1])
 
-        vector_.print_by_distance(points_by_distance)
+        Linear_Algebra.print_by_distance(points_by_distance)
 
         temp = []
         count_ = 0
@@ -130,7 +160,7 @@ class vector_ :
 #---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
-def plot_points(points_by_distance, v_to_classify, k):
+def plot_points(points_by_distance, v_to_classify, k):#
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
     
@@ -168,6 +198,8 @@ def plot_points(points_by_distance, v_to_classify, k):
 #---------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------------------------
+# loading the Iris dataset and tuning it so that it is compatible with this code 
+#---------------------------------------------------------------------------------------------------------------------------
 def load_iris_dataset():
     iris = load_iris()
     X = iris.data
@@ -184,7 +216,7 @@ def create_vectors_from_dataset(X, y):
     for i in range(len(X)):
         x, y_coord, z = X[i][0], X[i][1], X[i][2]
         classification = int(y[i])
-        vector_(x, y_coord, z, classification)
+        Linear_Algebra(x, y_coord, z, classification)
     return points
 
 def test_knn_accuracy(X_test, y_test, k=3, distance_mode=0):
@@ -192,13 +224,13 @@ def test_knn_accuracy(X_test, y_test, k=3, distance_mode=0):
     correct = 0
     total = len(X_test)
     for i in range(total):
-        test_point = vector_.__new__(vector_)
+        test_point = Linear_Algebra.__new__(Linear_Algebra)
         test_point.x = X_test[i][0]
         test_point.y = X_test[i][1]
         test_point.z = X_test[i][2]
         test_point.classification = None
         
-        predicted_class, _ = vector_.KNN(k=k, v_to_classify=test_point, points=points, distance_mode=distance_mode)
+        predicted_class, _ = Linear_Algebra.KNN(k=k, v_to_classify=test_point, points=points, distance_mode=distance_mode)
         
         if predicted_class == int(y_test[i]):
             correct += 1
@@ -211,29 +243,29 @@ def test_knn_accuracy(X_test, y_test, k=3, distance_mode=0):
 def __main__() -> None :
     # print("!!!!")
 
-    # v1 = vector_(1,1,1 , 0)
-    # v2 = vector_(2,2,2 , 0)
-    # v3 = vector_(3,3,3 , 0)
+    # v1 = Linear_Algebra(1,1,1 , 0)
+    # v2 = Linear_Algebra(2,2,2 , 0)
+    # v3 = Linear_Algebra(3,3,3 , 0)
 
-    # v4 = vector_(10,10,10 , 1)
-    # v5 = vector_(11,11,11 , 1)
-    # v6 = vector_(13,13,13 , 1)
+    # v4 = Linear_Algebra(10,10,10 , 1)
+    # v5 = Linear_Algebra(11,11,11 , 1)
+    # v6 = Linear_Algebra(13,13,13 , 1)
 
-    # v7 = vector_(1,0,0 , 0)
-    # v8 = vector_(0,0,0 , 0)
-    # v9 = vector_(0,1,1 , 0)
+    # v7 = Linear_Algebra(1,0,0 , 0)
+    # v8 = Linear_Algebra(0,0,0 , 0)
+    # v9 = Linear_Algebra(0,1,1 , 0)
 
-    # v10 = vector_(6,6,6 , 2)
-    # v11 = vector_(7,6,6 , 2)
-    # v12 = vector_(8,7,6 , 2)
+    # v10 = Linear_Algebra(6,6,6 , 2)
+    # v11 = Linear_Algebra(7,6,6 , 2)
+    # v12 = Linear_Algebra(8,7,6 , 2)
 
-    # # v_in = vector_(0.1,0.1,0.1)
-    # v_in = vector_(9,9,9)
-    # # v_in = vector_(6,7,7)
+    # # v_in = Linear_Algebra(0.1,0.1,0.1)
+    # v_in = Linear_Algebra(9,9,9)
+    # # v_in = Linear_Algebra(6,7,7)
 
     # k_input = 3
 
-    # v_in.classification , points_by_distance = vector_.KNN(k=k_input , v_to_classify=v_in , points=points)
+    # v_in.classification , points_by_distance = Linear_Algebra.KNN(k=k_input , v_to_classify=v_in , points=points)
     
     # print(v_in.classification)
 
@@ -247,8 +279,8 @@ def __main__() -> None :
     X_train, X_test, y_train, y_test = load_iris_dataset()
     create_vectors_from_dataset(X_train, y_train)
     
-    test_point = vector_(X_test[0][0], X_test[0][1], X_test[0][2])
-    predicted, points_by_distance = vector_.KNN(k=k_input, v_to_classify=test_point, points=points , distance_mode=distance_mode)
+    test_point = Linear_Algebra(X_test[0][0], X_test[0][1], X_test[0][2])
+    predicted, points_by_distance = Linear_Algebra.KNN(k=k_input, v_to_classify=test_point, points=points , distance_mode=distance_mode)
     
     print(f"\nPredicted class: {predicted}")
     print(f"Actual class: {int(y_test[0])}")
